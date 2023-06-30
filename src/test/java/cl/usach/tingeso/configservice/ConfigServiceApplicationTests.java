b78b450e6d1a81592679dcd1860f37ace6cbd1b6
Jovanni Schneider <jovanni.schneider@usach.cl>
2023-06-30 02:23:23 -0600

    Config-service first commit

---
+++ src/test/java/cl/usach/tingeso/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.tingeso.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
